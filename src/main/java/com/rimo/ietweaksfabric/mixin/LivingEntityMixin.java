78ee5efe8f469e998979e26e1fe32a861a9641d9
Rimo <113169310+Rivmun@users.noreply.github.com>
2025-06-22 02:47:15 +0800

    port to 1.21.6
    Fix #5

--- src/main/java/com/rimo/ietweaksfabric/mixin/LivingEntityMixin.java
+++ src/main/java/com/rimo/ietweaksfabric/mixin/LivingEntityMixin.java
@@ -3,7 +3,6 @@ package com.rimo.ietweaksfabric.mixin;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.ZombieEntity;
@@ -14,6 +13,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
@@ -24,12 +24,12 @@ public abstract class LivingEntityMixin {
 		 * Horse 20% faster.
 		 * (IDEA mark these lines as warning, just ignored... or add a parameter to catch the 'instanceof' result. But it's no sense.)
 		 */
-		if (((LivingEntity) (Object) this) instanceof AbstractHorseEntity a && attribute.getKey().equals(EntityAttributes.GENERIC_MOVEMENT_SPEED.getKey()))
+		if (((LivingEntity) (Object) this) instanceof AbstractHorseEntity a && attribute.getKey().equals(EntityAttributes.MOVEMENT_SPEED.getKey()))
 			cir.setReturnValue(cir.getReturnValue() * 1.2f);
 		/*
 		 * Zombie 20% unknockbackable.
 		 */
-		if (((LivingEntity) (Object) this) instanceof ZombieEntity a && attribute.getKey().equals(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE.getKey()))
+		if (((LivingEntity) (Object) this) instanceof ZombieEntity a && attribute.getKey().equals(EntityAttributes.KNOCKBACK_RESISTANCE.getKey()))
 			cir.setReturnValue(cir.getReturnValue() + 0.2f);
 	}
 
@@ -39,9 +39,9 @@ public abstract class LivingEntityMixin {
 	/*
 	 * Player get blindness when damaged over 6 points health.
 	 */
-	@Inject(at = @At("HEAD"), method = "damage")
-	public void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-		if (((LivingEntity) (Object) this) instanceof ServerPlayerEntity player && player.networkHandler != null && amount / player.getMaxHealth() > 0.33f)
+	@Inject(at = @At("HEAD"), method = "setHealth")
+	public void setHealth(float health, CallbackInfo ci) {
+		if (((LivingEntity) (Object) this) instanceof ServerPlayerEntity player && player.networkHandler != null && (player.getHealth() - health) / player.getMaxHealth() > 0.33f)
 			this.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 60));
 	}
 
@@ -49,7 +49,7 @@ public abstract class LivingEntityMixin {
 	 * Player get slowness when get fall damage.
 	 */
 	@Inject(at = @At("RETURN"), method = "computeFallDamage")
-	public void computeFallDamage(float fallDistance, float damageMultiplier, CallbackInfoReturnable<Integer> cir) {
+	public void computeFallDamage(double fallDistance, float damageMultiplier, CallbackInfoReturnable<Integer> cir) {
 		int i = cir.getReturnValue();
 		if (i > 0)
 			this.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, Math.max(60, i * 10), Math.min(4, i / 4)));
--- src/main/java/com/rimo/ietweaksfabric/mixin/ServerWorldMixin.java
+++ src/main/java/com/rimo/ietweaksfabric/mixin/ServerWorldMixin.java
@@ -4,7 +4,6 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.profiler.Profiler;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.world.GameRules;
@@ -13,24 +12,26 @@ import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 import java.util.function.BooleanSupplier;
-import java.util.function.Supplier;
 
 @Mixin(ServerWorld.class)
 public abstract class ServerWorldMixin extends World {
 
-	protected ServerWorldMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, DynamicRegistryManager registryManager, RegistryEntry<DimensionType> dimensionEntry, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long biomeAccess, int maxChainedNeighborUpdates) {
-		super(properties, registryRef, registryManager, dimensionEntry, profiler, isClient, debugWorld, biomeAccess, maxChainedNeighborUpdates);
+
+	protected ServerWorldMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, DynamicRegistryManager registryManager, RegistryEntry<DimensionType> dimensionEntry, boolean isClient, boolean debugWorld, long seed, int maxChainedNeighborUpdates) {
+		super(properties, registryRef, registryManager, dimensionEntry, isClient, debugWorld, seed, maxChainedNeighborUpdates);
 	}
 
 	@Shadow
 	public abstract List<ServerPlayerEntity> getPlayers();
 
+	@Unique
 	long time;
 
 	@Inject(at = @At("HEAD"), method = "tick")
@@ -43,7 +44,7 @@ public abstract class ServerWorldMixin extends World {
 	 */
 	@Inject(at = @At("HEAD"), method = "wakeSleepingPlayers")
 	private void wakeSleepingPlayers(CallbackInfo ci) {
-		float consume = this.getGameRules().getBoolean(GameRules.DO_DAYLIGHT_CYCLE) ?
+		float consume = this.getServer().getGameRules().getBoolean(GameRules.DO_DAYLIGHT_CYCLE) ?
 				10 * (24000L - (time % 24000L)) / 12000F :
 				6F;
 		this.getPlayers().stream().filter(LivingEntity::isSleeping).toList().forEach((player) -> {
